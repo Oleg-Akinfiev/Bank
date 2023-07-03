e1107685aec09e622d0cc4adf349512ac79d3c96
Oleg Akinfiev <akinfiev91@gmail.com>
2023-07-03 21:37:45 +0300

    commit

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,23 +9,21 @@ const Testimonials = () => {
 			id="clients"
 			className={`${styles.paragraph} ${styles.flexCenter} flex-col relative`}
 		>
-			<div />
-
 			<div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 relative z=[1]">
 				<h2 className={styles.heading2}>
 					What People are <br className="sm:block hidden" /> saying about us
 				</h2>
 				<div className="w-full md:mt-0 mt-6">
-					<p className={`${styles.paragraph} text-left max-w-[450px]:`}>
+					<p className={`${styles.paragraph} text-left max-w-[450px]`}>
 						Everything you need to accept card payments and grow your business
 						anywhere on the planet.
 					</p>
 				</div>
-				<div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
-					{feedback.map((card) => (
-						<FeedbackCard key={card.id} {...card} />
-					))}
-				</div>
+			</div>
+			<div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+				{feedback.map((card) => (
+					<FeedbackCard key={card.id} {...card} />
+				))}
 			</div>
 		</section>
 	);
