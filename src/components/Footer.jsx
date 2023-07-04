295c6323ed0a0ac6c4ffc9c5138727b8cb70d29a
Oleg Akinfiev <akinfiev91@gmail.com>
2023-07-04 21:42:01 +0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styles from '../style'
 
 const Footer = () => {
   return (
-	 <div>Footer</div>
+	 <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+		<div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+			<div className='flex-[1] flex-col justify-start mr-10'>
+				
+			</div>
+		</div>
+	 </section>
   )
 }
 
